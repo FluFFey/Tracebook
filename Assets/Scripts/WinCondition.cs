21a87293266e48321fc0906420565e573b5f6a55
Claus <claus.bugge@hotmail.com>
2018-04-17 13:45:10 +0200

    Added a proper wincondition. add script to a gameobject with a collider

--- Assets/Scripts/WinCondition.cs
+++ Assets/Scripts/WinCondition.cs
@@ -1,20 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Networking;
+
 using UnityEngine;
 
-public class WinCondition : MonoBehaviour {
+public class WinCondition : NetworkBehaviour {
 
+    BoxCollider2D collider;
+    List<GameObject> players = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
-		
+        collider = GetComponent<BoxCollider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-        if(Input.GetKeyDown(KeyCode.O))
+
+        if (isServer)
         {
-            MyServerManager.instance.changeScene("Discussion");
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                MyServerManager.instance.changeScene("Discussion");
+            }
+
+            var connectionCount = MyServerManager.instance.getConnectionCount();
+            if (players.Count == MyServerManager.instance.getConnectionCount())
+            {
+                MyServerManager.instance.changeScene("Discussion");
+            }
         }
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            players.Add(collision.gameObject);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            players.Remove(collision.gameObject);
+        }
+    }
+
 }
